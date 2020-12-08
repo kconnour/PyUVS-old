eaec91487e4eb2112ba27c304d3d5b54d29e9e4e
kyle <kdconey1@gmail.com>
2020-12-08 13:57:48 -0700

    Add setup script and documentation

--- setup.py
+++ setup.py
@@ -8,11 +8,15 @@ def setup_package():
         description='Make MAVEN/IUVS data more easily accessible',
         url='https://github.com/kconnour/maven-iuvs',
         author='kconnour',
+        packages=setuptools.find_packages(),
+        include_package_data=True,
         python_requires='>=3.8',
         install_requires=[
             'astropy>=4.2',
             'numpy>=1.19.4',
+            'pdoc3>=0.9.1',
         ],
+
     )
 
 
--- tests/hello.py
+++ tests/hello.py
@@ -1,2 +1,1 @@
-me = 'kyle'
-print(f'hello {me}')
+
