87029f3a3b5bda73494b021ec55d30d6033842cc
Kyle <kdconey1@gmail.com>
2020-12-28 18:49:30 -0600

    Add warnings to ScienceWeek
    Also PEP8 compliance.

--- maven_iuvs/science_week/science_week.py
+++ maven_iuvs/science_week/science_week.py
@@ -7,23 +7,19 @@ import numpy as np
 
 
 class ScienceWeek:
-    # TODO: Decide if I want to prohibit users from inputting negative science weeks, or dates that result in them
-    # TODO: Decide how to handle future science week (should I warn if they request science week from a future date?)
-    def __init__(self):
-        """ A ScienceWeek object can convert dates into MAVEN science weeks.
-
-        Properties
-        ----------
-        science_start_date: datetime.date
-            The date when IUVS began performing science.
-        """
-        self.__science_start_date = date(2014, 11, 11)
-
+    """ A ScienceWeek object can convert dates into MAVEN science weeks. """
     @property
     def science_start_date(self):
-        return self.__science_start_date
+        """ Get the date MAVEN/IUVS began performing science.
 
-    def get_science_week_from_date(self, some_date):
+        Returns
+        -------
+        date: datetime.date
+            The date MAVEN/IUVS began performing science.
+        """
+        return date(2014, 11, 11)
+
+    def week_from_date(self, some_date):
         """ Get the science week number at an input date.
 
         Parameters
@@ -37,12 +33,12 @@ class ScienceWeek:
             The science week at the input date.
         """
         try:
-            science_week = (some_date - self.__science_start_date).days // 7
-            return science_week
+            self.__warn_if_date_is_before_mission_start(some_date)
+            return (some_date - self.science_start_date).days // 7
         except TypeError:
-            raise TypeError('some_date should be of type datetime.date')
+            raise TypeError('"some_date" should be of type datetime.date')
 
-    def get_current_science_week(self):
+    def current_science_week(self):
         """ Get the science week number for today.
 
         Returns
@@ -50,11 +46,10 @@ class ScienceWeek:
         science_week: int
             The current science week.
         """
-        science_week = self.get_science_week_from_date(date.today())
-        return science_week
+        return self.week_from_date(date.today())
 
-    def get_science_week_start_date(self, week):
-        """ Get the date when the requested science week began.
+    def week_start_date(self, week):
+        """ Get the date when the requested science week began or will begin.
 
         Parameters
         ----------
@@ -67,16 +62,19 @@ class ScienceWeek:
             The date when the science week started.
         """
         try:
+            self.__warn_if_week_is_negative(week)
             rounded_week = int(np.floor(week))
             if week != rounded_week:
-                warnings.warn('This is a non-integer week. Converting it to integer...')
-            science_week_start = self.__science_start_date + timedelta(days=rounded_week * 7)
+                warnings.warn('This is a non-integer week. '
+                              'Converting it to integer...')
+            science_week_start = self.science_start_date + \
+                                 timedelta(days=rounded_week * 7)
             return science_week_start
         except TypeError:
-            raise TypeError(f'week should be an int, not a {type(week)}.')
+            raise TypeError(f'"week" should be an int, not a {type(week)}.')
 
-    def get_science_week_end_date(self, week):
-        """ Get the date when the requested science week ended.
+    def week_end_date(self, week):
+        """ Get the date when the requested science week ended or will end.
 
         Parameters
         ----------
@@ -88,9 +86,9 @@ class ScienceWeek:
         science_week_end: datetime.date
             The date when the science week ended.
         """
-        return self.get_science_week_start_date(week + 1) - timedelta(days=1)
+        return self.week_start_date(week) + timedelta(days=6)
 
-    def get_science_week_date_range(self, week):
+    def week_date_range(self, week):
         """ Get the date range corresponding to the input science week.
 
         Parameters
@@ -103,10 +101,13 @@ class ScienceWeek:
         date_range: tuple
             The start and end dates of the science week.
         """
-        date_range = self.get_science_week_start_date(week), self.get_science_week_end_date(week)
-        return date_range
+        return self.week_start_date(week), self.week_end_date(week)
 
+    def __warn_if_date_is_before_mission_start(self, some_date):
+        if (some_date - self.science_start_date).days < 0:
+            warnings.warn('The input date is before the science start date.')
 
-# TODO: Decide if I want a subclass to handle requests related to science week (ex. get_orbit_range_from_science_week)
-# Pro: It's easy to code after making the database and it'd be helpful
-# Con: It might be easier to make 1 utility for database searching and tell users to apply it to science week
+    @staticmethod
+    def __warn_if_week_is_negative(week):
+        if week < 0:
+            warnings.warn('The input week should not be negative.')
--- maven_iuvs/science_week/tests/test_science_week.py
+++ maven_iuvs/science_week/tests/test_science_week.py
@@ -15,37 +15,48 @@ class TestScienceWeek(TestCase):
         self.science_week = ScienceWeek()
 
 
-class TestScienceWeekInit(TestScienceWeek):
-    def test_science_week_has_science_start_date_attribute(self):
+# TODO: I'm unsure how to write general class checks if there's no constructor.
+class TestInit(TestScienceWeek):
+    def test_science_week_has_science_start_date_property(self):
         self.assertTrue(hasattr(self.science_week, 'science_start_date'))
 
-    def test_science_week_has_exactly_one_parameter(self):
-        self.assertTrue(len(self.science_week.__dict__.keys()), 1)
+    def test_science_week_has_no_attributes(self):
+        self.assertEqual(0, len(self.science_week.__dict__.keys()))
 
+
+class TestScienceStartDate(TestScienceWeek):
     def test_science_start_date_is_2014_11_11(self):
-        self.assertEqual(date(2014, 11, 11), self.science_week.science_start_date)
+        self.assertEqual(date(2014, 11, 11),
+                         self.science_week.science_start_date)
 
-    def test_science_start_date_cannot_be_modified(self):
+    def test_science_start_date_is_read_only(self):
         with self.assertRaises(AttributeError):
             self.science_week.science_start_date = date(2014, 11, 11)
 
 
-class TestGetScienceWeekFromDate(TestScienceWeek):
+class TestWeekFromDate(TestScienceWeek):
     def test_science_start_date_is_week_0(self):
-        self.assertEqual(0, self.science_week.get_science_week_from_date(self.science_week.science_start_date))
+        self.assertEqual(0, self.science_week.week_from_date(
+            self.science_week.science_start_date))
 
     def test_known_science_week_and_date_matches_output(self):
         test_date = date(2020, 12, 14)
-        self.assertEqual(317, self.science_week.get_science_week_from_date(test_date))
+        self.assertEqual(317, self.science_week.week_from_date(test_date))
 
     def test_integer_input_raises_type_error(self):
         with self.assertRaises(TypeError):
-            self.science_week.get_science_week_from_date(100)
+            self.science_week.week_from_date(100)
+
+    def test_date_before_mission_start_raises_warning(self):
+        with warnings.catch_warnings(record=True) as warning:
+            warnings.simplefilter("always")
+            self.science_week.week_from_date(date(2000, 1, 1))
+            self.assertEqual(1, len(warning))
+            self.assertEqual(warning[-1].category, UserWarning)
 
 
-# I cannot figure out how to properly mock a date such that ScienceWeek knows about my mock
+# TODO: Figure out a test for this
 class TestGetCurrentScienceWeek(TestScienceWeek):
-    # TODO: Get a test for this
     pass
     '''def test_science_week_of_today(self):
         with mock.patch('maven_iuvs.science_week.science_week.datetime.date') as mock_date:
@@ -54,96 +65,123 @@ class TestGetCurrentScienceWeek(TestScienceWeek):
             self.assertEqual(268, self.science_week.get_current_science_week())  # This doesn't about mock_date'''
 
 
-class TestGetScienceWeekStartDate(TestScienceWeek):
+class TestWeekStartDate(TestScienceWeek):
     def test_start_of_week_0_is_mission_arrival_date(self):
-        self.assertEqual(self.science_week.science_start_date, self.science_week.get_science_week_start_date(0))
+        self.assertEqual(self.science_week.science_start_date,
+                         self.science_week.week_start_date(0))
 
     def test_example_science_week_matches_its_known_start_date(self):
-        self.assertEqual(date(2020, 12, 8), self.science_week.get_science_week_start_date(317))
+        self.assertEqual(date(2020, 12, 8),
+                         self.science_week.week_start_date(317))
 
-    def test_floats_between_two_consecutive_integers_give_output_equal_to_lower_integer(self):
+    def test_floats_between_two_consecutive_integers_give_lower_output(self):
         with warnings.catch_warnings(record=True):
-            self.assertEqual(self.science_week.get_science_week_start_date(317),
-                             self.science_week.get_science_week_start_date(317.0),
-                             self.science_week.get_science_week_start_date(np.nextafter(318, 317)))
+            self.assertEqual(self.science_week.week_start_date(317),
+                             self.science_week.week_start_date(317.0),
+                             self.science_week.week_start_date(
+                                 np.nextafter(318, 317)))
 
     def test_integer_float_week_raises_no_warnings(self):
         with warnings.catch_warnings(record=True) as warning:
             warnings.simplefilter("always")
-            self.science_week.get_science_week_start_date(1.0)
+            self.science_week.week_start_date(1.0)
             self.assertEqual(0, len(warning))
 
     def test_non_integer_float_raises_warning(self):
         with warnings.catch_warnings(record=True) as warning:
             warnings.simplefilter("always")
-            self.science_week.get_science_week_start_date(1.5)
+            self.science_week.week_start_date(1.5)
             self.assertEqual(1, len(warning))
             self.assertEqual(warning[-1].category, UserWarning)
 
-    def test_ndarray_input_raises_type_error(self):
+    def test_ndarray_input_raises_value_error(self):
         test_weeks = np.linspace(1, 50, num=50, dtype=int)
-        with self.assertRaises(TypeError):
-            self.science_week.get_science_week_start_date(test_weeks)
+        with self.assertRaises(ValueError):
+            self.science_week.week_start_date(test_weeks)
 
     def test_string_input_raises_type_error(self):
         with self.assertRaises(TypeError):
-            self.science_week.get_science_week_start_date('100')
+            self.science_week.week_start_date('100')
 
     def test_nan_input_raises_value_error(self):
         with self.assertRaises(ValueError):
-            self.science_week.get_science_week_start_date(np.nan)
+            self.science_week.week_start_date(np.nan)
+
+    def test_negative_week_raises_warning(self):
+        with warnings.catch_warnings(record=True) as warning:
+            warnings.simplefilter("always")
+            self.science_week.week_start_date(-1)
+            self.assertEqual(1, len(warning))
+            self.assertEqual(warning[-1].category, UserWarning)
 
 
-class TestGetScienceWeekEndDate(TestScienceWeek):
+class TestWeekEndDate(TestScienceWeek):
     def test_end_of_week_0_is_6_days_after_science_start_date(self):
-        self.assertEqual(self.science_week.science_start_date + timedelta(days=6),
-                         self.science_week.get_science_week_end_date(0))
+        self.assertEqual(self.science_week.science_start_date +
+                         timedelta(days=6), self.science_week.week_end_date(0))
 
     def test_example_science_week_matches_its_known_end_date(self):
-        self.assertEqual(date(2020, 12, 14), self.science_week.get_science_week_end_date(317))
+        self.assertEqual(date(2020, 12, 14),
+                         self.science_week.week_end_date(317))
 
-    def test_floats_between_two_consecutive_integers_give_output_equal_to_lower_integer(self):
+    def test_floats_between_two_consecutive_integers_give_lower_output(self):
         with warnings.catch_warnings(record=True):
-            self.assertEqual(self.science_week.get_science_week_end_date(317),
-                             self.science_week.get_science_week_end_date(317.0),
-                             self.science_week.get_science_week_end_date(np.nextafter(318, 317)))
+            self.assertEqual(self.science_week.week_end_date(317),
+                             self.science_week.week_end_date(317.0),
+                             self.science_week.week_end_date(
+                                 np.nextafter(318, 317)))
 
     def test_integer_float_week_raises_no_warnings(self):
         with warnings.catch_warnings(record=True) as warning:
             warnings.simplefilter("always")
-            self.science_week.get_science_week_end_date(1.0)
+            self.science_week.week_end_date(1.0)
             self.assertEqual(0, len(warning))
 
     def test_non_integer_float_raises_warning(self):
         with warnings.catch_warnings(record=True) as warning:
             warnings.simplefilter("always")
-            self.science_week.get_science_week_end_date(1.5)
+            self.science_week.week_end_date(1.5)
             self.assertEqual(1, len(warning))
             self.assertEqual(warning[-1].category, UserWarning)
 
-    def test_ndarray_input_raises_type_error(self):
+    def test_ndarray_input_raises_value_error(self):
         test_weeks = np.linspace(1, 50, num=50, dtype=int)
-        with self.assertRaises(TypeError):
-            self.science_week.get_science_week_end_date(test_weeks)
+        with self.assertRaises(ValueError):
+            self.science_week.week_end_date(test_weeks)
 
     def test_string_input_raises_type_error(self):
         with self.assertRaises(TypeError):
-            self.science_week.get_science_week_end_date('100')
+            self.science_week.week_end_date('100')
 
     def test_nan_input_raises_value_error(self):
         with self.assertRaises(ValueError):
-            self.science_week.get_science_week_end_date(np.nan)
+            self.science_week.week_end_date(np.nan)
+
+    def test_negative_week_raises_warning(self):
+        with warnings.catch_warnings(record=True) as warning:
+            warnings.simplefilter("always")
+            self.science_week.week_end_date(-1)
+            self.assertEqual(1, len(warning))
+            self.assertEqual(warning[-1].category, UserWarning)
 
 
-class TestGetScienceWeekDateRange(TestScienceWeek):
+class TestWeekDateRange(TestScienceWeek):
     def test_output_is_tuple(self):
-        self.assertTrue(isinstance(self.science_week.get_science_week_date_range(100), tuple))
+        self.assertTrue(isinstance(self.science_week.week_date_range(100),
+                                   tuple))
 
     def test_output_is_2_elements(self):
-        self.assertTrue(len(self.science_week.get_science_week_date_range(100)), 2)
+        self.assertTrue(len(self.science_week.week_date_range(100)), 2)
 
     def test_output_is_result_of_start_and_end_date_methods(self):
-        self.assertTrue(self.science_week.get_science_week_start_date(100),
-                        self.science_week.get_science_week_date_range(100)[0])
-        self.assertTrue(self.science_week.get_science_week_end_date(100),
-                        self.science_week.get_science_week_date_range(100)[1])
+        self.assertTrue(self.science_week.week_start_date(100),
+                        self.science_week.week_date_range(100)[0])
+        self.assertTrue(self.science_week.week_end_date(100),
+                        self.science_week.week_date_range(100)[1])
+
+    def test_negative_week_throws_1_warning_by_default(self):
+        with warnings.catch_warnings(record=True) as warning:
+            warnings.simplefilter("default")
+            self.science_week.week_date_range(-1)
+            self.assertEqual(1, len(warning))
+            self.assertEqual(warning[-1].category, UserWarning)
