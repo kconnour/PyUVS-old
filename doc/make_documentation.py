e452df217ecd88e6a4f25fa8d465c479a2c1005f
kyle <kdconey1@gmail.com>
2021-01-22 13:59:23 -0700

    test

--- doc/make_documentation.py
+++ doc/make_documentation.py
@@ -3,3 +3,4 @@ import os
 package_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..',))
 module_path = os.path.join(package_path, 'pyuvs')
 os.system(f'/home/kyle/repos/maven-iuvs/venv/bin/pdoc --html {module_path} --force')
+# Test
