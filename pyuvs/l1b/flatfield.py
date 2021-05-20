0745ce44235f87dda5dd7c32a130b3658c192eec
kyle <kdconey1@gmail.com>
2021-05-20 13:03:03 -0600

    Add the mutable flatfield

--- pyuvs/l1b/flatfield.py
+++ pyuvs/l1b/flatfield.py
@@ -1,43 +1,22 @@
-'''
-This needs to be a mutable flatfield object. This was the old one
+import numpy as np
+from pyuvs.anc.flatfield import Flatfield, FlatfieldWavelengths
 
-class MUVFlatfield(_AuxiliaryArray):
-    """Read in the standard MUV flatfield.
 
-    This class will read in the standard MUV flatfield (133 positions,
-    19 wavelengths) unless another file is specified. It otherwise acts like a
-    numpy.ndarray. It contains methods to interpolate to another binning
-    scheme if necessary.
+class MUVFlatfield:
+    """Read in and manipulate the flatfield.
 
-    Parameters
-    ----------
-    file_path
-       Absolute path of the array to read in.
-
-    Raises
-    ------
-    TypeError
-        Raised if :code:`file_path` is not a string or None.
-    ValueError
-        Raised if :code:`file_path` does not have a .npy extension.
+    This class reads in the standard mid-ultraviolet flatfield. It provides
+    methods to interpolate it to a different binning scheme, as well as a
+    different wavelength grid.
 
     """
-    def __init__(self, file_path=None):
-        file_path = self.__make_file_path(file_path)
-        super().__init__(file_path)
-        self.__wavelengths = self.__get_flatfield_wavelengths()
-        self.__n_positions = self.array.shape[0]
-        self.__n_wavelengths = self.array.shape[1]
+    def __init__(self):
+        self.__ff = Flatfield()
+        self.__wavelengths = FlatfieldWavelengths()
+        self.__n_positions = self.__ff.shape[0]
+        self.__n_wavelengths = self.__ff.shape[1]
 
-    @staticmethod
-    def __make_file_path(file) -> str:
-        return file if file is not None else \
-            os.path.join(os.getcwd(), 'muv_flatfield.npy')
-
-    @staticmethod
-    def __get_flatfield_wavelengths() -> _AuxiliaryArray:
-        path = os.path.join(os.getcwd(), 'muv_flatfield_wavelengths.npy')
-        return _AuxiliaryArray(path)
+        self.__val = np.copy(self.__ff)
 
     def interpolate_to_new_scheme(self, n_positions: int,
                                   wavelengths: np.ndarray) -> None:
@@ -51,12 +30,12 @@ class MUVFlatfield(_AuxiliaryArray):
             The new wavelengths.
 
         """
-        self.array = self.__interpolate_to_positions(
+        self.__val = self.__interpolate_to_positions(
             n_positions, self.__interpolate_to_new_wavelengths(wavelengths))
 
     def __interpolate_to_new_wavelengths(self, wavs: np.ndarray) -> np.ndarray:
         return np.array(
-            [[np.interp(w, self.__wavelengths, self[pos, :]) for w in wavs]
+            [[np.interp(w, self.__wavelengths, self.val[pos, :]) for w in wavs]
              for pos in range(self.__n_positions)])
 
     def __interpolate_to_positions(self, n_positions: int,
@@ -69,4 +48,10 @@ class MUVFlatfield(_AuxiliaryArray):
             [[np.interp(pos, original_positions, trimmed_ff[:, wav])
               for wav in range(trimmed_ff.shape[-1])] for pos in new_positions]
         )
-'''
+
+    @property
+    def val(self):
+        """ Get the value of the transformed flatfield.
+
+        """
+        return self.__val
