add46841d8de142855be907059cc3b7c22b7f4c2
Kyle <kdconey1@gmail.com>
2022-01-25 21:36:16 -0700

    Change module names

---
+++ pyuvs/graphics/detector_image.py
@@ -0,0 +1,152 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from pyuvs.graphics.colorize import turn_primary_to_3_channels, \
+    histogram_equalize_rgb_image
+from pyuvs.swath import swath_number
+
+
+def reshape_data_for_pcolormesh(image: np.ndarray):
+    """Reshape an image array for use in pcolormesh.
+
+    Parameters
+    ----------
+    image
+        Any MxNx3 array.
+
+    Returns
+    -------
+    np.ndarray
+        Array with reshaped dimensions.
+
+    """
+    return np.reshape(image, (image.shape[0] * image.shape[1], image.shape[2]))
+
+
+def make_swath_grid(mirror_angles: np.ndarray, swath_number: int,
+                    n_positions: int, n_integrations: int) \
+        -> tuple[np.ndarray, np.ndarray]:
+    """Make a swath grid of mirror angles and spatial bins.
+
+    Parameters
+    ----------
+    mirror_angles
+    swath_number
+    n_positions
+    n_integrations
+
+    Returns
+    -------
+
+    """
+    slit_angles = np.linspace(angular_slit_width * swath_number,
+                              angular_slit_width * (swath_number + 1),
+                              num=n_positions+1)
+    mirror_angles = np.linspace(mirror_angles[0], mirror_angles[-1],
+                                num=n_integrations + 1)
+    return np.meshgrid(slit_angles, mirror_angles)
+
+
+def make_plot_fill(altitude_mask: np.ndarray) -> np.ndarray:
+    """Make the dummy plot fill required for pcolormesh
+
+    Parameters
+    ----------
+    altitude_mask
+
+    Returns
+    -------
+
+    """
+    return np.where(altitude_mask, 1, np.nan)
+
+
+def plot_detector_image(axis: plt.Axes, x: np.ndarray, y: np.ndarray,
+                        fill: np.ndarray, colors: np.ndarray) -> None:
+    """Plot a detector image created via custom color scheme.
+
+    Parameters
+    ----------
+    axis
+    x
+    y
+    fill
+    colors
+
+    Returns
+    -------
+
+    """
+    axis.pcolormesh(x, y, fill, color=colors, linewidth=0,
+                    edgecolors='none').set_array(None)
+
+
+if __name__ == '__main__':
+    from pyuvs.data_files import L1bFile, \
+        find_latest_apoapse_muv_file_paths_from_block, stack_dayside_primaries, \
+        stack_nightside_primaries, make_dayside_altitude_mask, \
+        stack_mirror_angles, make_dayside_integration_mask
+    from pyuvs.constants import angular_slit_width, minimum_mirror_angle, maximum_mirror_angle
+    from pathlib import Path
+    import time
+    from pyuvs.swath import select_integrations_in_swath
+    from scipy.io import readsav
+
+    # Flatfields
+    flatfields = ['MIDRESAPO_FLATFIELD_19X50_ORBIT03733_3739.sav', 'MIDRESAPO_FLATFIELD_19X50_ORBIT03744_3750.sav']
+
+    # Read in data
+    p = Path('/media/kyle/Samsung_T5/IUVS_data')
+    orbits = [3734, 3741, 3744, 3748]
+    for o in orbits:
+        print(o)
+        files = find_latest_apoapse_muv_file_paths_from_block(p, o)
+        files = [L1bFile(f) for f in files]
+        #t0 = time.time()
+
+        if o in [3744, 3748]:
+            flip = True
+            ff = readsav(f'/Users/kyco2464/Downloads/{flatfields[1]}')
+            ff = ff['midresapo_flatfield_19x50_orbit03744_3750']
+        else:
+            flip = False
+            ff = readsav(f'/Users/kyco2464/Downloads/{flatfields[0]}')
+            ff = ff['midresapo_flatfield_19x50_orbit03733_3739']
+
+        # Make data that I need
+        dayside_primary = stack_dayside_primaries(files)
+        nightside_primary = stack_nightside_primaries(files)
+        dayside_altitude_mask = make_dayside_altitude_mask(files)
+        mirror_angles = stack_mirror_angles(files)
+        swath_numbers = swath_number(mirror_angles)
+        dayside_integration_mask = make_dayside_integration_mask(files)
+        dayside_mirror_angles = mirror_angles[dayside_integration_mask]
+
+        # Flatfield correct
+        ff = np.broadcast_to(ff, dayside_primary.shape)
+        dayside_primary = dayside_primary / ff
+
+        # Color transform
+        coadded_primary = turn_primary_to_3_channels(dayside_primary)
+        heq_primary = histogram_equalize_rgb_image(coadded_primary, mask=dayside_altitude_mask) / 255
+        #t1 = time.time()
+
+        # Try out the graphics
+        fig, ax = plt.subplots()
+        ax.set_xlim(0, 6 * angular_slit_width)
+        ax.set_ylim(minimum_mirror_angle, maximum_mirror_angle)
+
+        for i in np.unique(swath_numbers):
+            # Get the colorized array + angles of this swath
+            swath_rgb = select_integrations_in_swath(heq_primary, i, swath_numbers, dayside_integration_mask)
+            swath_mirror_angles = select_integrations_in_swath(dayside_mirror_angles, i, swath_numbers, dayside_integration_mask)
+            swath_altitude_mask = select_integrations_in_swath(dayside_altitude_mask, i, swath_numbers, dayside_integration_mask)
+            fill = make_plot_fill(swath_altitude_mask)
+            x, y = make_swath_grid(swath_mirror_angles, i, swath_rgb.shape[1], swath_rgb.shape[0])   # mirror angles, swath number, n_pos, n_int
+            swath_rgb = reshape_data_for_pcolormesh(swath_rgb)
+            plot_detector_image(ax, x, y, fill, swath_rgb)
+            #plot_custom_primary(ax, x, y, fill, foo)
+        plt.savefig(f'/Users/kyco2464/ql_testing/testql{o}.png')
+        #t2 = time.time()
+
+        #print(t2-t1, t1-t0)
+
--- pyuvs/swath.py
+++ pyuvs/swath.py
@@ -1,3 +1,6 @@
+"""
+
+"""
 import numpy as np
 
 
@@ -33,12 +36,64 @@ def swath_number(mirror_angles: np.ndarray) -> np.ndarray:
     return np.floor(interp_swaths).astype('int')
 
 
-def count_integrations_in_swath(swath_numbers: np.ndarray, mask: np.ndarray):
+def count_integrations_in_swath(
+        swath_numbers: np.ndarray, mask: np.ndarray) -> np.ndarray:
+    """Count the number of integrations that fall in each swath number given
+    a mask that selects integrations of interest.
+
+    Parameters
+    ----------
+    swath_numbers: np.ndarray
+        1D array of the swath numbers of each integration.
+    mask: np.ndarray
+        1D array of booleans, where :code:`True` values indicate integrations
+        of interest. Must be the same shape as :code:`swath_numbers`.
+
+    Returns
+    -------
+    np.ndarray
+        The number of integrations in each swath that meet the mask's criteria.
+
+    Examples
+    --------
+    Count the number of dayside integrations in a fictionalized set of swath
+    numbers.
+
+    >>> from pyuvs.swath import count_integrations_in_swath
+    >>> swath_numbers = np.array([0, 0, 0, 1, 1, 1])
+    >>> dayside_mask = np.array([True, True, False, True, False, False])
+    >>> count_integrations_in_swath(swath_numbers, dayside_mask)
+    array([2, 1])
+
+    """
     edges = np.concatenate([[-1], np.unique(swath_numbers)]) + 0.5
     return np.histogram(swath_numbers[mask], bins=edges)[0]
 
 
-def select_info_from_swath(array: np.ndarray, swath_number: int, swath_numbers: np.ndarray, mask: np.ndarray):
-    integrations_in_swath = count_integrations_in_swath(swath_numbers, mask)
+def select_integrations_in_swath(
+        array: np.ndarray, swath_number: int, swath_numbers: np.ndarray,
+        integrations_per_swath: np.ndarray) -> np.ndarray:
+    """Select only the data from some array given a desired swath number.
+
+    Parameters
+    ----------
+    array: np.ndarray
+        Any array. The 0th axis is assumed to be the integration axis.
+    swath_number
+        The swath number to select data from.
+    swath_numbers
+        1D array of swath numbers.
+    integrations_per_swath
+        1D array of the number of integrations corresponding to
+        :code:`swath_numbers`.
+
+    Returns
+    -------
+    np.ndarray
+        :code:`array`, but trimmed.
+
+    """
+    integrations_in_swath = \
+        count_integrations_in_swath(swath_numbers, integrations_per_swath)
     edge_inds = np.concatenate([[0], np.cumsum(integrations_in_swath)])
     return array[edge_inds[swath_number]: edge_inds[swath_number + 1], ...]
